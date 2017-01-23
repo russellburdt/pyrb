d226492d3ba1ec3324fcb207cb19858b5cf38cb0
Russell Burdt <russellburdt@gmail.com>
2017-01-22 22:12:25 -0800

    bokeh update

--- bokeh.py
+++ bokeh.py
@@ -4,17 +4,43 @@ short bokeh utility methods
 """
 
 
-from ipdb import set_trace
+def column(*args, **kwargs):
+    """
+    replace bokeh.layouts.column with a special case of bokeh.layouts.gridplot
 
+    gridplot is meant to be called with a list of lists that define a grid:
+    e.g. 'gridplot([[plot_1, plot_2], [plot_3, plot_4]])'
 
-def linkfigs(figs):
+    a nice side effect of gridplot is just 1 toolbar is used, whereas in a
+    column layout every figure gets its own toolbar (major clutter)
 
-    set_trace()
+    this column method uses the special case of gridplot for just 1 column
+    of figures, thus replacing the standard column layout
+    """
 
+    from bokeh.layouts import _handle_children
+    from bokeh.layouts import gridplot
 
-    # at this point I am going to need a utility function to link plots
-    # e.g. pyrb.linkfigs(figs, axis='x'), it will do:
-    # figs[1].x_range = figs[0].x_range
-    # figs[2].x_range = figs[0].x_range
-    # ..., and same thing for axis='y' or axis='xy'
-    # otherwise this all must be managed in figure method calls
+    # get a list of figure objects from *args, return a gridplot as a column plot
+    figs = _handle_children(*args)
+    return gridplot([[x] for x in figs], **kwargs)
+
+def linkfigs(*args, axis='x'):
+    """
+    link axes for a list of bokeh figures
+    'axis' is either 'x', 'y', or 'xy'
+    """
+
+    from bokeh.layouts import _handle_children
+
+    # get a list of figure objects from *args, scan over them
+    figs = _handle_children(*args)
+    for fig in figs[1:]:
+
+        # link x axes for all figures if requested
+        if 'x' in axis:
+            fig.x_range = figs[0].x_range
+
+        # link y axes for all figures if requested
+        if 'y' in axis:
+            fig.y_range = figs[0].y_range
