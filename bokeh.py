f60a40cb85fc81e0fda21bdd941ba3aa11d959f7
Russell Burdt <russellburdt@gmail.com>
2017-02-01 21:01:11 -0800

    more

--- bokeh.py
+++ bokeh.py
@@ -3,6 +3,63 @@
 short bokeh utility methods
 """
 
+from bokeh.models.widgets import Slider
+class SliderWithButtons(Slider):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # from ipdb import set_trace
+        # set_trace()
+
+def slider_with_buttons(width=300, dim=40, **kwargs):
+    """
+    return a bokeh layout object representing a slider widget with button widgets
+    on each side that increment and decrement the slider value by step
+    the slider 'on_change' method can be accessed from the returned object as:
+
+    ** 29 Jan 2017, this does not work for a few reasons:
+        the on_change method of the slider is not easily accessed by the
+        calling method as the returned object is a Row layout, and the slider
+        object is a child of a widgetbox making up that layout, i.e. it is
+        available as: returned_object.children[1].children[0].on_change
+
+        callbacks linked the slider are not working as bokeh thinks the 'value'
+        attribute is that of the returned Row layout, and this does not have a
+        'value' attribute
+
+        the proper way to create this widget is to create a new Slider class
+        that properly links the bokeh Slider and Button classes, but this is
+        difficult as it is not really clear how the bokeh Slider class is
+        created (it inherits a bokeh class that inherits a bokeh class...)
+    **
+    """
+
+    from bokeh.models.widgets import Slider, Button
+    from bokeh.layouts import widgetbox, row
+
+    # initialize widget objects
+    start = kwargs.pop('start', None)
+    end = kwargs.pop('end', None)
+    step = kwargs.pop('step', None)
+    value = kwargs.pop('value', None)
+    slider = Slider(start=start, end=end, step=step, value=value, **kwargs)
+    minus = Button(label='-')
+    plus = Button(label='+')
+
+    # create callbacks that update the slider on a button press
+    def plus_callback():
+        slider.value += slider.step
+    def minus_callback():
+        slider.value -= slider.step
+
+    # set the event handlers
+    minus.on_click(minus_callback)
+    plus.on_click(plus_callback)
+
+    # return a row layout made up of the slider and button widgets
+    return row(
+        widgetbox(minus, width=dim, height=dim, sizing_mode='fixed'),
+        widgetbox(slider, width=width, height=dim, sizing_mode='fixed'),
+        widgetbox(plus, width=dim, height=dim, sizing_mode='fixed'))
 
 def open_figure(N=10, cmap='plasma'):
     """
@@ -10,8 +67,14 @@ def open_figure(N=10, cmap='plasma'):
     the idea is to return up to N glyph objects that can later be populated
     with data, but already have the colormap set
     """
-    pass
+    return
 
+    from bokeh.plotting import figure
+
+    # initialize a bokeh figure object
+    fig = figure()
+    glyphs = None
+    return fig, glyphs
 
 def column(*args, **kwargs):
     """
