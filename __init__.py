4b952b3f2228636167c215ce324fff10a9276f4e
Russell Burdt <russell.burdt@asml.com>
2018-05-21 10:51:44 -0700

    'ok'

---
+++ __init__.py
@@ -0,0 +1,40 @@
+
+"""
+define hooks to some functions in other modules,
+so existing tools referenced to pyrb continue to work
+"""
+
+# hooks to pyrb.mpl
+from pyrb import mpl
+addspecline = mpl.addspecline
+format_axes = mpl.format_axes
+get_current_figs = mpl.get_current_figs
+get_random_closed_data_marker = mpl.get_random_closed_data_marker
+largefonts = mpl.largefonts
+maximize_figs = mpl.maximize_figs
+open_figure = mpl.open_figure
+save_pngs = mpl.save_pngs
+subplots_adjust = mpl.subplots_adjust
+thicklines = mpl.thicklines
+update_figs = mpl.update_figs
+
+# hooks to pyrb.processing
+from pyrb import processing
+arange = processing.arange
+create_filename = processing.create_filename
+get_unambiguous_tz = processing.get_unambiguous_tz
+get_utc_times = processing.get_utc_times
+is_writeable = processing.is_writeable
+linspace = processing.linspace
+loadmat = processing.loadmat
+longest_substring_from_list = processing.longest_substring_from_list
+matlab2datetime = processing.matlab2datetime
+memoized = processing.memoized
+movingaverage = processing.movingaverage
+movingstd = processing.movingstd
+numpy_datetime64_to_datetime = processing.numpy_datetime64_to_datetime
+pngs2ppt = processing.pngs2ppt
+return_aliased_freq = processing.return_aliased_freq
+return_dict = processing.return_dict
+run_notch_filter_example = processing.run_notch_filter_example
+search_tree = processing.search_tree
