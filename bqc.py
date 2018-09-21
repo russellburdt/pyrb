50548348f982ae690a7329a3a98c8989ac45a2c4
Russell B <russellburdt@gmail.com>
2018-09-21 16:26:47 -0700

    'ok

--- bqc.py
+++ bqc.py
@@ -1,8 +1,8 @@
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import pydotplus
+import datasets
 from collections import Counter, defaultdict
 from pyrb import open_figure, format_axes, largefonts, save_pngs
 from pyrb.knuth import algorithm_u_permutations
@@ -21,7 +21,6 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn import metrics
-from sklearn import datasets
 plt.style.use('bmh')
 
 
@@ -29,75 +28,46 @@ plt.style.use('bmh')
 improve classification accuracy with a clustering pre-processing step
 """
 
-# get valid sklearn classification datasets
-valid = ['iris', 'digits', 'wine', 'breast_cancer']
-df_datasets = defaultdict(list)
-for ds in valid:
-    data = getattr(datasets, 'load_' + ds)()
-    df_datasets['name'].append(ds)
-    df_datasets['Xshape'].append(data.data.shape)
-    df_datasets['Xtype'].append(data.data.dtype)
-    df_datasets['yshape'].append(data.target.shape)
-    df_datasets['ytype'].append(data.target.dtype)
-    df_datasets['# unique y'].append(np.unique(data.target).size)
-    try:
-        df_datasets['y names'].append(', '.join(data.target_names))
-    except TypeError:
-        df_datasets['y names'].append(', '.join(data.target_names.astype(np.str)))
-df_datasets = pd.DataFrame(index=df_datasets.pop('name'), data=df_datasets)
+data = datasets.supervised_mammographic_mass()
+X, y = data['X'].values, data['y'].values
 
-# apply classifiers to each dataset
-classifiers = [GaussianNB, SVC, RandomForestClassifier, KNeighborsClassifier, LogisticRegression]
-for ds in valid:
-    print('\n')
-    data = getattr(datasets, 'load_' + ds)()
-    X = data.data
-    y = data.target
-    for classifier in classifiers:
+# get baseline classification accuracy
+classifiers = [GaussianNB,
+               SVC,
+               RandomForestClassifier,
+               KNeighborsClassifier,
+               LogisticRegression,
+               DecisionTreeClassifier]
+accuracy = []
+for classifier in classifiers:
+    classifier = classifier()
+    accuracy.append(cross_val_score(classifier, X, y, cv=5).mean())
 
-        classifier = classifier()
-        scores = cross_val_score(classifier, X, y, cv=10)
-        print('10-fold cross-validation accuracy, mean {:.2f}%, stdev {:.2f}%'.format(
-            100 * scores.mean(), 100 * scores.std()))
+# determine best number of clusters with the elbow method
+elbow = []
+for n in range(1, 20):
+    kmm = cluster.MiniBatchKMeans(n_clusters=n)
+    kmm.fit(X)
+    elbow.append(kmm.inertia_)
+n_clusters = 5
+
+# get cluster assignments based on optimal n_clusters in previous step
+kmm = cluster.KMeans(n_clusters=n_clusters)
+kmm.fit(X)
+ycluster = kmm.predict(X)
 
 
 
 
-# # make blob data
-# centers = 4
-# X, y = datasets.make_blobs(n_samples=20000, n_features=5, centers=centers, cluster_std=0.2)
 
-# # map blog data to 2D space
-# pca = PCA(n_components=2)
-# pca.fit(X)
-# X = pca.transform(X)
+# accuracy = []
+# groups = list(algorithm_u_permutations(ns=np.unique(df['bq type cluster']), m=np.unique(df['bq type int']).size))
+# for group in tqdm(groups, desc='scanning groups'):
+#     tmp = np.full(df['bq type int'].size, np.nan)
+#     for idx, items in enumerate(group):
+#         tmp[df['bq type cluster'].isin(items)] = idx
+#     accuracy.append(metrics.accuracy_score(df['bq type int'], tmp))
 
-# # plot blob data
-# fig, ax = open_figure('blobs', figsize=(8, 5))
-# for center in range(centers):
-#     idx = (y == center)
-#     ax.plot(X[idx, 0], X[idx, 1], 'x', label='blob {}'.format(center))
-# ax.legend(loc='upper left', bbox_to_anchor=(1, 1), numpoints=3)
-# largefonts(14)
-# fig.tight_layout()
-# plt.show()
 
-# # convert y into 'centers' or fewer classes
-# yclass = np.empty(y.size, dtype=np.object)
-# yclass[y == 0] = 'class 0 or 1'
-# yclass[y == 1] = 'class 0 or 1'
-# yclass[y == 2] = 'class 2 or 3'
-# yclass[y == 3] = 'class 2 or 3'
-# # yclass[y == 4] = 'class 4 or 5'
-# # yclass[y == 5] = 'class 4 or 5'
 
-# # convert yclass into encoded data
-# le = LabelEncoder()
-# le.fit(yclass)
-# yclass = le.transform(yclass)
 
-# # fit a decision tree to the results
-# classifier = DecisionTreeClassifier(max_depth=3)
-# scores = cross_val_score(classifier, X, y, cv=5)
-# print('10-fold cross-validation accuracy, mean {:.2f}%, stdev {:.2f}%'.format(
-#     100 * scores.mean(), 100 * scores.std()))
--- datasets.py
+++ datasets.py
@@ -15,13 +15,15 @@ dataset returns the following consistent information.
 --- supervised datasets return a dictionary of
     * text description - description of data from source location
     * X0 - DataFrame of original feature data and column names
-     (categorical and numeric formats if applicable)
     * y0 - Series of original class data with name
-    * X - DataFrame with all categorical data encoded to numeric data
-    * y - Series with categorical data encoded to numeric
-    * encoders - dict of all LabelEncoder objects used in X0 to X and y0 to y transformations
+    (X0 and y0 represent unclean, original data - categorical, numeric, missing, etc. all as in original source)
+    * X - DataFrame with all data cleaning steps applied
+    * y - Series with all data cleaning steps applied
+    (see code to understand cleaning steps)
+    * encoders - dict of any LabelEncoder objects used in data cleaning
 
 --- regression datasets return ...
+
 --- unsupervised datasets return ...
 
 Current data sources are
@@ -35,7 +37,10 @@ R Burdt, v0.01
 import requests
 import numpy as np
 import pandas as pd
+from itertools import chain
+from collections import Counter
 from sklearn.preprocessing import LabelEncoder
+from sklearn.datasets import load_wine, load_digits
 from ipdb import set_trace
 
 
@@ -47,6 +52,7 @@ def supervised_ecoli():
 
     # initialize an output dictionary, assign a text description to the dataset
     out = {}
+    out['name'] = 'ECOLI'
     out['text description'] = \
         requests.get(r'https://archive.ics.uci.edu/ml/machine-learning-databases/ecoli/ecoli.names').text
 
@@ -63,22 +69,98 @@ def supervised_ecoli():
     out['X0'] = pd.DataFrame(data=X, columns=['Sequence Name', 'mcg', 'gvh', 'lip', 'chg', 'aac', 'alm1', 'alm2'])
     out['y0'] = pd.Series(data=y, name='localization site')
 
-    # convert categorical data to numeric, save LabelEncoders in a dict
+    # create LabelEncoder objects, save to out
+    le1 = LabelEncoder()
+    le1.fit(out['X0']['Sequence Name'])
+    le2 = LabelEncoder()
+    le2.fit(out['y0'])
     out['encoders'] = {}
+    out['encoders']['X0, Sequence Name'] = le1
+    out['encoders']['y0'] = le2
 
-    le = LabelEncoder()
-    le.fit(out['X0']['Sequence Name'])
-    out['encoders']['X0, Sequence Name'] = le
-
-    le = LabelEncoder()
-    le.fit(out['y0'])
-    out['encoders']['y0'] = le
-
-    # create X, y data with only numeric data
+    # create clean X, y data
     out['X'] = out['X0'].copy()
-    out['X']['Sequence Name'] = out['encoders']['X0, Sequence Name'].transform(out['X0']['Sequence Name'])
+    out['X']['Sequence Name'] = le1.transform(out['X']['Sequence Name'])
     out['X'] = out['X'].apply(pd.to_numeric)
-    out['y'] = pd.Series(data=out['encoders']['y0'].transform(out['y0'].values), name=out['y0'].name)
+    out['y'] = pd.Series(data=le2.transform(out['y0'].values), name=out['y0'].name)
+    assert pd.isnull(out['X'].values).sum() == 0
+    assert pd.isnull(out['y']).sum() == 0
+
+    return out
+
+def supervised_lung_cancer():
+    """
+    retrieve information from UCI Machine Learning Lung Cancer dataset
+    https://archive.ics.uci.edu/ml/datasets/Lung+Cancer
+    """
+
+    # initialize an output dictionary, assign a text description to the dataset
+    out = {}
+    out['name'] = 'LUNG CANCER'
+    out['text description'] = \
+        requests.get(r'https://archive.ics.uci.edu/ml/machine-learning-databases/lung-cancer/lung-cancer.names').text
+
+    # read data from url; parse to X, y numpy arrays
+    data = requests.get(r'https://archive.ics.uci.edu/ml/machine-learning-databases/lung-cancer/lung-cancer.data').text
+    data = data.split('\n')[:-2]
+    data = [x.split(',') for x in data]
+    assert np.unique([len(x) for x in data]) == 57
+    data = np.array(data)
+    X = data[:, 1:]
+    y = data[:, 0]
+
+    # original data, including all original numeric and categorical values
+    # (there are no feature labels for this dataset)
+    out['X0'] = pd.DataFrame(data=X, columns=['feature{}'.format(x) for x in range(56)])
+    out['y0'] = pd.Series(data=y, name='type of lung cancer')
+
+    # create clean X, y data
+    out['X'] = out['X0'].copy()
+    out['X'][out['X'] == '?'] = -1
+    out['X'] = out['X'].apply(pd.to_numeric)
+    out['y'] = pd.to_numeric(out['y0'].copy())
+    assert pd.isnull(out['X'].values).sum() == 0
+    assert pd.isnull(out['y']).sum() == 0
+
+    return out
+
+def supervised_mammographic_mass():
+    """
+    retrieve information from UCI Machine Learning Mammographic Mass dataset
+    https://archive.ics.uci.edu/ml/datasets/Mammographic+Mass
+    """
+
+    # initialize an output dictionary, assign a text description to the dataset
+    out = {}
+    out['name'] = 'MAMMOGRAPHIC MASS'
+    out['text description'] = \
+        requests.get(r'https://archive.ics.uci.edu/ml/machine-learning-databases/mammographic-masses/mammographic_masses.names').text
+
+    # read data from url; parse to X, y numpy arrays
+    data = requests.get(r'https://archive.ics.uci.edu/ml/machine-learning-databases/mammographic-masses/mammographic_masses.data').text
+    data = data.split('\n')[:-2]
+    data = [x.split(',') for x in data]
+    data = [[x.strip() for x in y] for y in data]
+    assert np.unique([len(x) for x in data]) == 6
+    data = np.array(data)
+    X = data[:, :-1]
+    y = data[:, -1]
+
+    # original data, including all original numeric and categorical values
+    out['X0'] = pd.DataFrame(data=X, columns=['bi-rads', 'age', 'shape', 'margin', 'density'])
+    out['y0'] = pd.Series(data=y, name='benign or malignant')
+
+    # create clean X, y data
+    # this dataset has mostly a missing value problem
+    out['X'] = out['X0'].copy()
+    for name, col in out['X'].iteritems():
+        cavg = np.array(col)
+        cavg = cavg[cavg != '?'].astype(np.float).mean()
+        out['X'].loc[col == '?', name] = cavg
+    out['X'] = out['X'].apply(pd.to_numeric)
+    out['y'] = pd.to_numeric(out['y0'].copy())
+    assert pd.isnull(out['X'].values).sum() == 0
+    assert pd.isnull(out['y']).sum() == 0
 
     return out
 
@@ -90,6 +172,7 @@ def supervised_ionosphere():
 
     # initialize an output dictionary, assign a text description to the dataset
     out = {}
+    out['name'] = 'IONOSPHERE'
     out['text description'] = \
         requests.get(r'https://archive.ics.uci.edu/ml/machine-learning-databases/ionosphere/ionosphere.names').text
 
@@ -99,15 +182,124 @@ def supervised_ionosphere():
     data = [x.split(',') for x in data]
     assert np.unique([len(x) for x in data]) == 35
     data = np.array(data)
-
-    set_trace()
-
     X = data[:, :-1]
     y = data[:, -1]
 
+    # original data, including all original numeric and categorical values
+    columns = list(chain(*[['pulse {} real'.format(x), 'pulse {} imag'.format(x)] for x in range(17)]))
+    out['X0'] = pd.DataFrame(data=X, columns=columns)
+    out['y0'] = pd.Series(data=y, name='reading good or bad')
 
+    # convert categorical data to numeric, save LabelEncoders in a dict
+    le = LabelEncoder()
+    le.fit(out['y0'])
+    out['encoders'] = {}
+    out['encoders']['y0'] = le
 
+    # create X, y data with only numeric data
+    out['X'] = out['X0'].copy()
+    out['X'] = out['X'].apply(pd.to_numeric)
+    out['y'] = pd.Series(data=out['encoders']['y0'].transform(out['y0'].values), name=out['y0'].name)
+    assert pd.isnull(out['X'].values).sum() == 0
+    assert pd.isnull(out['y']).sum() == 0
 
+    return out
 
+def supervised_car_evaluation():
+    """
+    retrieve information from UCI Machine Learning Repository Car Evaluation dataset
+    https://archive.ics.uci.edu/ml/datasets/Car+Evaluation
+    """
 
+    # initialize an output dictionary, assign a text description to the dataset
+    out = {}
+    out['name'] = 'CAR EVALUATION'
+    out['text description'] = \
+        requests.get(r'https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.names').text
 
+    # read data from url; parse to X, y numpy arrays
+    data = requests.get(r'https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data').text
+    data = data.split('\n')[:-1]
+    data = [x.split(',') for x in data]
+    assert np.unique([len(x) for x in data]) == 7
+    data = np.array(data)
+    X = data[:, :-1]
+    y = data[:, -1]
+
+    # original data, including all original numeric and categorical values
+    columns = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety']
+    out['X0'] = pd.DataFrame(data=X, columns=columns)
+    out['y0'] = pd.Series(data=y, name='reading good or bad')
+
+    # this dataset requires LabelEncoders for all features and for class data
+    le_features = [LabelEncoder(), LabelEncoder(), LabelEncoder(), LabelEncoder(), LabelEncoder(), LabelEncoder()]
+    le = LabelEncoder()
+    out['encoders'] = {}
+    for le_feature, column in zip(le_features, columns):
+        le_feature.fit(out['X0'][column])
+        out['encoders']['X0, {}'.format(column)] = le_feature
+    le.fit(out['y0'])
+    out['encoders']['y0'] = le
+
+    # create clean X, y data
+    out['X'] = out['X0'].copy()
+    for le_feature, column in zip(le_features, columns):
+        out['X'][column] = le_feature.transform(out['X0'][column])
+    out['X'] = out['X'].apply(pd.to_numeric)
+    out['y'] = pd.Series(data=le.transform(out['y0'].values), name=out['y0'].name)
+    assert pd.isnull(out['X'].values).sum() == 0
+    assert pd.isnull(out['y']).sum() == 0
+
+    return out
+
+def supervised_digits():
+    """
+    retrieve information from scikit-learn built-in dataset
+    sklearn.datasets.load_digits
+    """
+
+    # load sklearn dataset
+    data = load_digits()
+
+    # initialize an output dictionary, assign a text description to the dataset
+    out = {}
+    out['name'] = 'DIGITS'
+    out['text description'] = data.DESCR
+
+    # load training and class data into DataFrames
+    out['X0'] = pd.DataFrame(data=data.data, columns=['pixel {}'.format(x) for x in range(64)])
+    out['y0'] = pd.Series(data=data.target, name='digit in range 0-9')
+
+    # create clean X, y data - no data cleaning required in this case
+    assert pd.isnull(out['X0'].values).sum() == 0
+    assert pd.isnull(out['y0']).sum() == 0
+    out['X'] = out['X0'].copy()
+    out['y'] = out['y0'].copy()
+
+    return out
+
+def supervised_wine():
+    """
+    retrieve information from scikit-learn built-in dataset
+    sklearn.datasets.load_digits
+    """
+
+    # load sklearn dataset
+    data = load_wine()
+
+    # initialize an output dictionary, assign a text description to the dataset
+    out = {}
+    out['name'] = 'WINE'
+    out['text description'] = data.DESCR
+
+    # load training and class data into DataFrames
+    out['X0'] = pd.DataFrame(data=data.data, columns=data.feature_names)
+    out['y0'] = pd.Series(data=data.target, name='wine type')
+
+    # create X, y data - no data cleaning required in this case
+    assert pd.isnull(out['X0'].values).sum() == 0
+    assert pd.isnull(out['y0']).sum() == 0
+    out['X'] = out['X0'].copy()
+    out['y'] = out['y0'].copy()
+
+    return out
