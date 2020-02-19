8c19fb249f806d6fe671428cc247d51d27adf9de
Russell Burdt <russellburdt@gmail.com>
2020-02-18 22:03:28 -0800

    ok

--- processing.py
+++ processing.py
@@ -31,32 +31,6 @@ def get_bounds_of_data_within_interval(data, x=0.95):
         else:
             return data[remove], data[-remove - 2]
 
-def get_most_likely_distribution_membership(x, d1, d2):
-    """
-    determine if a number x is more likely to belong to a distribution d1 or a distribution d2
-    - work in progress
-    """
-    import numpy as np
-    from numba import jit
-
-    # returns by how many standard deviations of ``dist`` is ``xi`` from the mean of ``dist``
-    @jit(nopython=True)
-    def distance(xi, dist):
-        if np.unique(dist).size == 1:
-            return np.inf
-        return np.abs(np.mean(dist) - xi) / np.std(dist)
-
-    # compare distance of x from d1 and d2
-    dist1 = distance(x, d1)
-    dist2 = distance(x, d2)
-    assert dist1 != dist2
-
-    # return 1 or 2 representing the closer distribution
-    if dist1 < dist2:
-        return 1
-    else:
-        return 2
-
 def convert_all_jpg_2_pdf(datadir):
     """
     convert all .jpg files in datadir to .pdf files with matching filenames
@@ -324,10 +298,10 @@ def movingstd(x, k):
     from numpy import array
 
     if x.ndim == 1:
-        return array(DataFrame(x).rolling(window=k, center=False,min_periods=1).std()).squeeze()
+        return array(DataFrame(x).rolling(window=k, center=False).std()).squeeze()
 
     elif x.ndim == 2:
-        return array(DataFrame(x).rolling(window=k, center=False,min_periods=1).std())
+        return array(DataFrame(x).rolling(window=k, center=False).std())
 
     else:
         raise ValueError('too many dims')
